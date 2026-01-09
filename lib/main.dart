318302fffe96d50071c599edb60fbbaa3abc122d
Chotbodin Siripol <chotbodin.av2547@gmail.com>
2026-01-09 14:55:30 +0700

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,64 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  String activeDice = 'assets/images/dice-1.png';
+
+  void rollDice() {
+    setState(() {
+      var dice = Random().nextInt(6) + 1;
+      activeDice = 'assets/images/dice-$dice.png';
+      print(activeDice);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.blue,
+                Colors.red,
+                Colors.green,
+              ],
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  activeDice,
+                  width: 200,
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    padding: EdgeInsets.all(16),
+                  ),
+                  onPressed: rollDice,
+                  child: Text("Roll Dice"),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
